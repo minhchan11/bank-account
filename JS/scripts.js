61a4ba124b1164bce432e70afbe314de4860e936
Jiwon Kang and Minh Phuong <jiwon@>
2017-01-24 13:28:12 -0800

    add output and constructor in business logic

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,13 @@
 //BUSINESS LOGIC
+function Account(initial,deposit,withdrawal) {
+  this.initial = initial;
+  this.deposit = deposit;
+  this.withdrawal = withdrawal;
+}
 //USER INTERFACE
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    
+
   });
 });
