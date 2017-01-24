5cad1d7fa55f3b36e879167c06288d787011e8de
Jiwon Kang and Minh Phuong <minh@>
2017-01-24 13:35:13 -0800

    add prototype to constructor to calculate total

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,18 @@
 //BUSINESS LOGIC
-function Account(initial,deposit,withdrawal) {
+var total = 0;
+//Constructor
+function Account(fullname,initial,deposit,withdrawal) {
+  this.fullname = fullname;
   this.initial = initial;
   this.deposit = deposit;
   this.withdrawal = withdrawal;
 }
+
+//Account Prototype for Total Amount of Funds
+Account.prototype.total = function() {
+  return total += this.initial += this.deposit -= this.withdrawal;
+}
+
 //USER INTERFACE
 $(document).ready(function(){
   $("form").submit(function(event){
